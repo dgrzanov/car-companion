a32e6709da4774ffb91006b19c4c3671e217972f
Dino Gržanov <dino.grzanov@gmail.com>
2022-10-12 12:33:49 +0200

    * list item icon and title

--- src/components/ExpenseListItem.tsx
+++ src/components/ExpenseListItem.tsx
@@ -1,15 +1,34 @@
-import { useLinkProps } from '@react-navigation/native';
 import * as React from 'react';
 import { Dimensions, StyleSheet, View } from 'react-native';
 import { Text } from 'react-native-paper';
 import { Avatar } from 'react-native-paper';
 
 function ExpenseListItem({ expense_type, cost, mileage }: any) {
+    const getTitle = (type: string) => {
+        switch(type) {
+            case "refuel":
+                return "Refuel"
+            case "service":
+                return "Service"
+        }
+    }
+
+    const getIcon = (type: string) => {
+        switch(type) {
+            case "refuel":
+                return "gas-station"
+            case "service":
+                return "wrench"
+            default:
+                return "help-circle"
+        }
+    }
+    
     return ( 
         <View style={styles.container}>
-            <Avatar.Icon size={50} icon="gas-station" />
+            <Avatar.Icon size={50} icon={getIcon(expense_type)} />
             <View>
-                <Text>{expense_type}</Text>
+                <Text>{getTitle(expense_type)}</Text>
                 <Text>{mileage} km</Text>
             </View>
             <Text>{cost} kn</Text>
